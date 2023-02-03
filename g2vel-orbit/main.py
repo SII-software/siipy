d22f95708ce301067122debf1a99ace48a61c086
Prasenjit Saha <psaha@physik.uzh.ch>
2023-02-03 10:44:46 +0100

    small changes

--- g2vel-orbit/main.py
+++ g2vel-orbit/main.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 
 from orbit import Orbit
 
-spica = Orbit(P=78.53, e=0.334, I=65.5,
+regor = Orbit(P=78.53, e=0.334, I=65.5,
               Omega=247.7, omega=67.7, jdperi=2400000.5+50120.4, q=9/28)
 
 
 jdp = 2400000.5+50120.4
-for jd in np.linspace(jdp,jdp+80,41):
-#for jd in np.linspace(2460000,2460080,81):
-    x,y = spica.binarypos(jd)
+#for jd in np.linspace(jdp,jdp+80,41):
+for jd in np.linspace(2460060,2460080,81):
+    x,y = regor.binarypos(jd)  # x is West, y is North
     R = [0.065,.023]
     plt.clf()
     plt.title('%.2f' % jd)
@@ -21,5 +21,5 @@ for jd in np.linspace(jdp,jdp+80,41):
     for k in 0,1:
         circ = plt.Circle((-x[k],-y[k]), R[k], color='r') # North is down, not up
         ax.add_patch(circ)
-    #plt.plot(x[0]-x[1],y[0]-y[1],'.')
+    #plt.plot(x[0]-x[1],y[0]-y[1],'.',color='black')
     plt.pause(0.04)
